73754d84991a2d405d8a488f8266a0e61faaec69
Khaled Monsoor <k@kmonsoor.com>
2014-12-16 15:17:19 +0600

    + initial fork, edit and commit

---
+++ generate_random_alphanum.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# # licensed under: CreativeCommons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+# # license details:  http://creativecommons.org/licenses/by-sa/3.0/
+#
+# # credits::   initial source:  http://stackoverflow.com/a/2257449/617185
+# # licensed under:  CC-SA-3.0   http://creativecommons.org/licenses/by-sa/3.0/
+
+"""
+    Returns a random string consisting only uppercase(A-Z) and numbers(0-9).
+
+    String length will be <length>
+"""
+from random import choice
+from string import ascii_uppercase, digits
+
+
+def generate_random_alphanum(length=None):
+    """
+    :param  length   length of the target random string
+    :returns Returns a random string consisting only uppercase(A-Z) and numbers(0-9)
+    """
+    return '' if (not length) else ''.join(choice(ascii_uppercase + digits) for _ in range(length))
+
